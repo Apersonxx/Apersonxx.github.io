fef7813fec1ec0dce9425510db05e2fae4b0de42
xiaoxiao <xiaoxiao.chan@iCloud.com>
2022-10-13 17:11:05 +0800

    bug修复

--- js/components.js
+++ js/components.js
@@ -1,15 +1,15 @@
 window.onload=function(){
     $('.header').append('<img src="https://tva1.sinaimg.cn/large/007X8olVly1g7hmb4jp4mj31kw07n7d8.jpg" class="img_h1"/>\n' +
-        '    <img src="https://cliclitv.cn/img/logo_cnicni.png" class="logo_cnicni"/>\n' +
+        '    <img src="img/logo_cnicni.png" class="logo_cnicni"/>\n' +
         '    <div class="bg_color">\n' +
         '        <div class="searcher">\n' +
-        '            <a href="https://cliclitv.cn/">\n' +
+        '            <a href="https://apersonxx.github.io/">\n' +
         '                <div class=\'logo_img\'>\n' +
-        '                    <img src="https://cliclitv.cn/img/logo-1.png">\n' +
+        '                    <img src="img/logo-1.png">\n' +
         '                </div>\n' +
         '                <span class="master_station">主站</span>\n' +
         '            </a>\n' +
-        '            <a style="margin-left: 20%" href="https://cliclitv.cn/app/money.html">\n' +
+        '            <a style="margin-left: 20%" href="https://apersonxx.github.io/app/money.html">\n' +
         '                <span class="master_station">赚钱花</span>\n' +
         '            </a>\n' +
         '        </div>\n' +
@@ -23,17 +23,17 @@ window.onload=function(){
         '\n' +
         '        <div class="down_app" id="dowl">\n' +
         '\n' +
-        '            <img src="https://cliclitv.cn/img/app.png">\n' +
+        '            <img src="img/app.png">\n' +
         '            <p>扫描下载</p>\n' +
         '            <p>手机客户端</p>\n' +
         '        </div>\n' +
         '        <!--导航-->\n' +
         '        <ul>\n' +
-        '            <li><a href="https://cliclitv.cn/">首页</a></li>\n' +
-        '            <li><a href="https://cliclitv.cn/login.html">\n' +
+        '            <li><a href="https://apersonxx.github.io/">首页</a></li>\n' +
+        '            <li><a href="https://apersonxx.github.io/login.html">\n' +
         '                <!--<img src="img/login.jpg" style="width: 30%;border-radius:50%;height:100%; margin-right: 2%;margin-top: -3%;border: 1px solid silver;">-->\n' +
         '                登录/注册</a></li>\n' +
-        '            <li class="on"><a href="https://cliclitv.cn/up.html" class="a_push"\n' +
+        '            <li class="on"><a href="https://apersonxx.github.io/up.html" class="a_push"\n' +
         '                              style="color: #fff;background-color: red;">投稿</a></li>\n' +
         '        </ul>\n' +
         '        <!--搜索-->\n' +
@@ -53,38 +53,38 @@ window.onload=function(){
         '</div>\n' +
         '<div class="header_img">\n' +
         '    <div class="left_img">\n' +
-        '        <a href="https://cliclitv.cn/video/cartoon/u2/cn00001.html">\n' +
+        '        <a href="https://apersonxx.github.io/video/cartoon/u2/cn00001.html">\n' +
         '            <img src="https://pic.mvodxc.com/pic/uploadimg/2018-12/61095.jpg">\n' +
         '        </a>\n' +
         '    </div>\n' +
-        '    <a href="https://cliclitv.cn/video/cartoon/u2/cn00001.html">命运之夜——天之杯：恶兆之花</a>\n' +
+        '    <a href="https://apersonxx.github.io/video/cartoon/u2/cn00001.html">命运之夜——天之杯：恶兆之花</a>\n' +
         '    <div class="video-count">xx播放&nbsp;&nbsp;xx弹幕</div>\n' +
         '</div>\n' +
         '<div class="header_img">\n' +
         '    <div class="left_img">\n' +
-        '        <a href="https://cliclitv.cn/video/cartoon/u1/cn00001.html">\n' +
+        '        <a href="https://apersonxx.github.io/video/cartoon/u1/cn00001.html">\n' +
         '            <img src="https://puui.qpic.cn/fans_admin/0/3_1359855289_1557544075974/0">\n' +
         '        </a>\n' +
         '    </div>\n' +
-        '    <a href="https://cliclitv.cn/video/cartoon/u1/cn00001.html">你的名字</a>\n' +
+        '    <a href="https://apersonxx.github.io/video/cartoon/u1/cn00001.html">你的名字</a>\n' +
         '    <div class="video-count">xx播放&nbsp;&nbsp;xx弹幕</div>\n' +
         '</div>\n' +
         '<div class="header_img">\n' +
         '    <div class="left_img">\n' +
-        '        <a href="https://cliclitv.cn/video/movie/u1/cn00001.html">\n' +
+        '        <a href="https://apersonxx.github.io/video/movie/u1/cn00001.html">\n' +
         '            <img src="https://cmdyimg.tjyjj.cn/upload/vod/2019-10/157028136620.jpg">\n' +
         '        </a>\n' +
         '    </div>\n' +
-        '    <a href="https://cliclitv.cn/video/movie/u1/cn00001.html">我和我的祖国</a>\n' +
+        '    <a href="https://apersonxx.github.io/video/movie/u1/cn00001.html">我和我的祖国</a>\n' +
         '    <div class="video-count">xx播放&nbsp;&nbsp;xx弹幕</div>\n' +
         '</div>\n' +
         '<div class="header_img">\n' +
         '    <div class="left_img">\n' +
-        '        <a href="https://cliclitv.cn/video/movie/u2/cn00001.html">\n' +
+        '        <a href="https://apersonxx.github.io/video/movie/u2/cn00001.html">\n' +
         '            <img src="https://cmdyimg.tjyjj.cn/upload/vod/2019-12/157692686818.jpg">\n' +
         '        </a>\n' +
         '    </div>\n' +
-        '    <a href="https://cliclitv.cn/video/movie/u2/cn00001.html">叶问4</a>\n' +
+        '    <a href="https://apersonxx.github.io/video/movie/u2/cn00001.html">叶问4</a>\n' +
         '    <div class="video-count">xx播放&nbsp;&nbsp;xx弹幕</div>\n' +
         '</div>')
 };
